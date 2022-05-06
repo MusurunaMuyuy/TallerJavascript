e07b9608b38f1f008ed162297e226e7ea26f3f13
Teffy90 <eocampol@unal.edu.co>
2022-05-06 11:22:31 -0500

    el punto 1 lo hizo Musuruna, el punto 2 se hizo en conjunto con el grupo

--- nuevo.js
+++ nuevo.js
@@ -8,3 +8,25 @@ for (var i = 0; i < inputs.length; i++){
         }
     });
 }
+
+function showContent() { 
+    if(document.getElementById ('flexRadioDefault1').checked){
+        
+        element = document.getElementById("content");
+        check = document.getElementById("check");
+        element.style.display='block';
+
+    }
+    if(document.getElementById ('flexRadioDefault2').checked){
+        alert("Sera en otra oportunidad")
+        element.style.display='none';
+    
+
+    }
+}
+function upperCase() {
+    var text = document.getElementById("nombre");
+    text.addEventListener("blur", blur, true);
+    if (text.checked == false)
+        alert(""+text.value.toUpperCase())
+ }
